4c753c163d901f056e9a6cca5998593780794746
shubham92000 <sbhsingh616@gmail.com>
2021-01-23 00:50:14 +0530

    exp1 js

---
+++ javascript/exp1.js
@@ -0,0 +1,5 @@
+var myb=document.querySelector("button");
+// myb.addEventListener("click",function(){myb.classList.add("gored");console.log("fuck")});
+myb.addEventListener("click",function(){document.querySelector("body").classList.toggle("gored");console.log("fuck")});
+
+// alert("connected");
